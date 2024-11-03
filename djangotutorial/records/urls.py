ba5a3b7d68ef740fd4e38a19ab5ef646e1943c79
Med Amine <ouarrakimohammed@gmail.com>
2024-11-03 11:50:59 +0100

    1 commit

--- djangotutorial/records/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.records_list, name="records_list"),  
-    path("<int:id>", views.record_detail, name="record_detail"),
-]
